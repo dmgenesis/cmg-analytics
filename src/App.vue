c66f86a268a57dab87495843b3bf4ae621ed5b02
dmgenesis - macPro <genes.printadds@gmail.com>
2020-08-11 16:30:07 +0800

    created tab header and functionality

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,70 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+   <div id="app-container">
+      <div class="container">
+         <ul id="tab-header">
+            <li 
+               class="tab-list"
+               :class="{ active : isLive }"
+               @click="isLive = true">
+               Active
+               </li>
+            <li 
+               class="tab-list"
+               :class="{ active : !isLive }"
+               @click="isLive = false">
+               Previous
+            </li>
+         </ul>
+         <div id="tab-content">
+         </div>
+      </div>
+   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      isLive : false
     }
+  },
+  components : {
+
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+   #app-container{
+      padding: 100px 0 60px;
+      background-color:#dee2e6;
+      min-height: 100vh;
+   }
+   ul#tab-header{
+      padding: 0;
+      margin:0;
+      display: flex;
+      list-style-type:none;
+   }
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+   ul#tab-header li.active{
+      background-color:#fff;
+      border-top:solid 3px #007bff;
+   }
+   ul#tab-header li{
+      padding: 10px 30px;
+      background-color: #F8F9F9;
+      border-top:solid 3px #fff;
+      margin: 0 5px 0 0;
+      color:#000;
+      text-decoration: none;
+      text-transform: uppercase;
+   }
+   ul#tab-header li:hover{
+      background-color:#fff;
+   }
+   #tab-content{
+      background-color:#fff;
+      padding:40px 25px;
+   }
 </style>
